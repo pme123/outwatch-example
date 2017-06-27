87076de35b35806b2ea17e8c3f5620b34aecc99f
pme123 <feensturm@drachensegler.ch>
2017-06-27 07:49:46 +0200

    Verified Filtering in UI.

--- src/main/scala/nextds/server/control/FilterTagCreator.scala
+++ src/main/scala/nextds/server/control/FilterTagCreator.scala
@@ -22,7 +22,9 @@ object FilterTagCreator {
     Seq(
       FilterTagConf(allSites.last, "DE AND (MGA OR CPF)", Seq("DE", "MGA", "CPF").map(createFilterTags.filterTag))
       , FilterTagConf(allSites.last, "(EN AND ORP) OR NJR", Seq("EN", "ORP", "NJR").map(createFilterTags.filterTag))
-      , FilterTagConf(allSites.last, "EN OR DE", Seq("EN").map(createFilterTags.filterTag))
+      , FilterTagConf(allSites.last, "EN OR DE", Seq("EN", "DE").map(createFilterTags.filterTag))
+      , FilterTagConf(allSites.last, "EN AND DE", Seq("EN", "DE").map(createFilterTags.filterTag))
+      , FilterTagConf(allSites.last, "(IT OR EN) AND DE", Seq("EN", "DE", "IT").map(createFilterTags.filterTag))
       , FilterTagConf(allSites.last, "EN", Seq("EN").map(createFilterTags.filterTag))
       , FilterTagConf(allSites.last, "DE", Seq("DE").map(createFilterTags.filterTag))
       , FilterTagConf(allSites.last, "FR", Seq("FR").map(createFilterTags.filterTag))
